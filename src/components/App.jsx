7830724f9ee48b1d442b440da833886aba1faaca
Leonid Holub <rsleonid@gmail.com>
2022-11-18 18:24:01 +0200

    Додає файли готового ДЗ.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
+// import { update } from '../redux/filterSlice';
+// import { nanoid } from 'nanoid';
+import { selectIsLoading, selectError } from 'redux/selectors';
+import Section from './Section/Section';
+import Form from './Form/Form';
+import Contacts from './Contacts/Contacs';
+
+export default function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Phonebook">
+        <Form />
+      </Section>
+      {isLoading && !error && (
+        <b style={{ display: 'block', color: 'white', padding: '25px 50px' }}>
+          Request in progress...
+        </b>
+      )}
+      {!isLoading && !error && (
+        <Section title="Contacts">
+          <Contacts />
+        </Section>
+      )}
+    </>
   );
-};
+}
